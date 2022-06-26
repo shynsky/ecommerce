5f7595000627ed1b7cd7f67cf4fd94d7f730dc05
marek.wituszynski@gmail.com <marek.wituszynski@gmail.com>
2022-06-26 19:16:23 +0200

    Navbar

---
+++ frontend/components/Nav.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import { FiShoppingBag } from 'react-icons/fi'
+import { NavStyle, NavItems } from '../styles/NavStyles'
+
+export default function Nav() {
+  return (
+    <NavStyle>
+      <Link href={'/'}>stl.</Link>
+      <NavItems>
+        <div>
+          <FiShoppingBag />
+          <h3>Cart</h3>
+        </div>
+      </NavItems>
+    </NavStyle>
+  )
+}
--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,12 +1,16 @@
 import { ProductStyles } from '../styles/ProductsStyle'
+import Link from 'next/link'
 
 export default function Products({ product }) {
-  const { title, price, image } = product.attributes
+  const { title, price, image, slug } = product.attributes
   return (
     <ProductStyles>
-      <div>
-        <img src={image.data.attributes.formats.small.url} alt='' />
-      </div>
+      <Link href={`/product/${slug}`}>
+        <div>
+          <img src={image.data.attributes.formats.small.url} alt='' />
+        </div>
+      </Link>
+
       <h2>{title}</h2>
       <h3>{price}</h3>
     </ProductStyles>
