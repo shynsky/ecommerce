4acd578f726615daac1a446706cf4d38f5c98c4e
marek.wituszynski@gmail.com <marek.wituszynski@gmail.com>
2022-06-26 13:28:56 +0200

    scaffolded backend, frontend and basic fetching with urql

---
+++ frontend/pages/_app.js
@@ -0,0 +1,13 @@
+import { createClient, Provider } from 'urql'
+
+const client = createClient({ url: 'http://localhost:1337/graphql' })
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Provider value={client}>
+      <Component {...pageProps} />
+    </Provider>
+  )
+}
+
+export default MyApp
