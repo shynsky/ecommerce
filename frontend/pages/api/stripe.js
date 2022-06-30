a8b3c6f6f98aaf0136b43dfab6a534816cc2ce23
marek.wituszynski@gmail.com <marek.wituszynski@gmail.com>
2022-06-30 18:15:37 +0200

    Stripe, cont

--- frontend/pages/api/stripe.js
+++ frontend/pages/api/stripe.js
@@ -12,6 +12,10 @@ export default async function handler(req, res) {
         shipping_address_collection: {
           allowed_countries: ['PL', 'US']
         },
+        allow_promotion_codes: true,
+        shipping_options: [
+          { shipping_rate: 'shr_1LGKomJKGLQ42yiDSyTZ6qBt' }, { shipping_rate: 'shr_1LGPz1JKGLQ42yiDN34s0Hvs' }
+        ],
         line_items: req.body.map(item => {
           return {
             price_data: {
@@ -21,6 +25,10 @@ export default async function handler(req, res) {
                 images: [item.image.data.attributes.formats.thumbnail.url]
               },
               unit_amount: item.price * 100
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1
             },
             quantity: item.quantity,
           }
