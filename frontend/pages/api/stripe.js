09244420fd478efc6f32580aecf838d7e650c3eb
marek.wituszynski@gmail.com <marek.wituszynski@gmail.com>
2022-07-01 06:51:24 +0200

    Auth0 setup

--- frontend/pages/api/stripe.js
+++ frontend/pages/api/stripe.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
           }
         }),
         // Bring ppl to the success or failure page
-        success_url: `${req.headers.origin}/success`,
+        success_url: `${req.headers.origin}/success?&session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${req.headers.origin}/canceled`,
       })
       res.status(200).json(session)
